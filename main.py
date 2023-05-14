29dc12e0712122ec98dd2b53466e42ee10f44bca
RoKaTG <msiliniyassine@gmail.com>
2023-05-15 00:17:19 +0200

    Brid class initialized

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ import neat as nt
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
 
-BIRD_IMG = [pg.transform.scale2x(pg.image.load(os.path.join("imgs","bird1.png"))), pg.transform.scale2x(pg.image.load(os.path.join("imgs","bird2.png"))), pg.transform.scale2x(pg.image.load(os.path.join("imgs","bird3.png")))]
+
+BIRD_IMG = [pg.transform.scale2x(pg.image.load(os.path.join("imgs","bird1.png"))),
+            pg.transform.scale2x(pg.image.load(os.path.join("imgs","bird2.png"))),
+            pg.transform.scale2x(pg.image.load(os.path.join("imgs","bird3.png")))]
 PIPE_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs","pipe.png")))
 BASE_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs","base.png")))
 BG_IMG = pg.transform.scale2x(pg.image.load(os.path.join("imgs","bg.png")))
+
+
+class Bird:
+    IMG = BIRD_IMG
+    MAX_ROTATION = 25
+    ROTATION_VELOCITY = 20
+    ANIMATION_TIME = 5
+
